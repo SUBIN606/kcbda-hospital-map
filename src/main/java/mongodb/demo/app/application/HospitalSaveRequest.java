4c6383c6f3ae6fb1cda760882d3b1d4569f1f683
subin606 <gaemilee606@gmail.com>
2022-08-10 10:23:58 +0900

    Hospital 엔티티로 변환하는 기본 메서드 추가

--- src/main/java/mongodb/demo/app/application/HospitalSaveRequest.java
+++ src/main/java/mongodb/demo/app/application/HospitalSaveRequest.java
@@ -1,6 +1,7 @@
 package mongodb.demo.app.application;
 
 import mongodb.demo.app.domain.Hospital;
+import mongodb.demo.app.domain.HospitalDocument;
 
 /** 신규 동물병원 등록 요청 */
 public interface HospitalSaveRequest {
@@ -11,6 +12,12 @@ public interface HospitalSaveRequest {
 
     double getY();
 
+    /** HospitalDocument로 변환 */
+    default HospitalDocument toHospitalDocument() {
+        return HospitalDocument.of(getName(), getX(), getY());
+    }
+
+    /** Hospital 엔티티로 변환 */
     default Hospital toHospital() {
         return Hospital.of(getName(), getX(), getY());
     }
