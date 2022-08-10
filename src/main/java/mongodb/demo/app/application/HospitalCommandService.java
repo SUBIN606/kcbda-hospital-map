d39ac2724013c4456f4d7ae104e58658bc7db7ba
subin606 <gaemilee606@gmail.com>
2022-08-10 10:25:58 +0900

    두 가지 데이터베이스 모두 저장되도록 수정
    동물병원 저장 시 두 가지 데이터베이스에 모두 저장되도록 repository 의존성을 추가하고, 저장합니다.

--- src/main/java/mongodb/demo/app/application/HospitalCommandService.java
+++ src/main/java/mongodb/demo/app/application/HospitalCommandService.java
@@ -1,20 +1,25 @@
 package mongodb.demo.app.application;
 
 import mongodb.demo.app.domain.Hospital;
+import mongodb.demo.app.repository.HospitalMongoRepository;
 import mongodb.demo.app.repository.HospitalRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HospitalCommandService implements HospitalSaveService {
 
-    private HospitalRepository repository;
+    private final HospitalMongoRepository mongoRepository;
+    private final HospitalRepository repository;
 
-    public HospitalCommandService(HospitalRepository repository) {
+    public HospitalCommandService(HospitalMongoRepository mongoRepository,
+                                  HospitalRepository repository) {
+        this.mongoRepository = mongoRepository;
         this.repository = repository;
     }
 
     @Override
     public Hospital saveHospital(HospitalSaveRequest hospital) {
+        mongoRepository.save(hospital.toHospitalDocument());
         return repository.save(hospital.toHospital());
     }
 
