5a8c2ee9e85f318eb45a4adb0076de6a40229438
subin606 <gaemilee606@gmail.com>
2022-08-19 14:06:40 +0900

    HospitalSaveDto에 기본 생성자 추가

--- src/main/java/mongodb/demo/app/controller/HospitalSaveController.java
+++ src/main/java/mongodb/demo/app/controller/HospitalSaveController.java
@@ -2,9 +2,11 @@ package mongodb.demo.app.controller;
 
 import mongodb.demo.app.application.HospitalCommandService;
 import mongodb.demo.app.application.HospitalSaveRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /** 신규 동물병원 등록 담당 컨트롤러 */
@@ -19,16 +21,20 @@ public class HospitalSaveController {
     }
 
     /** 신규 동물병원 등록 */
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public HospitalReadController.HospitalResponse saveHospital(@RequestBody HospitalSaveDto hospital) {
         return new HospitalReadController.HospitalResponse(service.saveHospital(hospital));
     }
 
-    /** 신규 동물병원 데이터를 저장 객체 */
+    /** 신규 동물병원 요청 데이터 */
     public static class HospitalSaveDto implements HospitalSaveRequest {
         private String name;
         private double x;
         private double y;
+
+        public HospitalSaveDto() {
+        }
 
         public HospitalSaveDto(String name, double x, double y) {
             this.name = name;
